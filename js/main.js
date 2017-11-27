e0fe5a3aad3095eca6558927005655317020210b
DESKTOP-M6SGJ3O\Skinnybuff <s_t_white@yahoo.com>
2017-11-27 15:33:47 -0500

    started on the game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["king", "king", "queen", "queen"];
+var cardsInPlay = [];
 
-console.log("Used fliped" + cardFour);
-console.log("Used fliped" + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipper " + cardsInPlay[0]);
+
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }else{
+        alert("Sorry try again");
+    }
+}
