4e6ff445dbfc0afbc6a7ee078b98566e30ccdd69
DESKTOP-M6SGJ3O\Skinnybuff <s_t_white@yahoo.com>
2017-11-29 01:00:49 -0500

    card objects created   file updated for card obj

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 
-var cards = ["king", "king", "queen", "queen"];
+var cards = [
+    {rank:"queen",
+    suit:"hearts",
+    cardImage: "img/queen-of-hearts.png"
+    },
+    {rank:"queen",
+    suit:"diamonds",
+    cardImage: "img/queen-of-diamonds.png"
+    },
+    {rank:"king",
+    suit:"hearts",
+    cardImage: "img/king-of-hearts.png"
+    },
+    {rank:"king",
+    suit:"diamonds",
+    cardImage: "img/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -11,8 +28,11 @@ var checkForMatch = function(){
 }
 
 var flipCard= function(cardId){
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+
+    cardsInPlay.push(cards[cardId].rank);
 
     checkForMatch();
 }
